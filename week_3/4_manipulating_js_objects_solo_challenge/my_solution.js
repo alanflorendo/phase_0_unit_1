1570f917d59db3165377c4b68b15b25867fb7b1a
Alan Florendo <alan.florendo@gmail.com>
2014-05-07 11:48:11 -0500

    update 3.4

--- week_3/4_manipulating_js_objects_solo_challenge/my_solution.js
+++ week_3/4_manipulating_js_objects_solo_challenge/my_solution.js
@@ -13,34 +13,58 @@ var terah = {
   eyeColor: "brown"
 }
 /* Pseudocode Section - write pseudocode for each challenge below.
+// NOTE THAT I DID TWO ROUNDS OF PSEUDO CODE--GENERALLY, I THINK MY SECOND ONE IS BETTER, AS
+// IT DOESN'T MERELY RESTATE THE CHALLENGE, BUT INSTEAD, FOCUSES ON TRANSLATING THE CHALLENGE
+// INTO CODE 
+
 1. Define a variable adam and use object literal notation to assign this variable 
    the value of a JavaScript Object object with no properties.
 // Create Adam object with no properties
+// Create adam object
+
 
 2. Give adam a name property with the value "Adam".
-// Give Adam object name of Adam
+// Give adam object name of Adam
+// Name adam object
+
 
 3. Add a spouse property to terah and assign it the value of adam.
+// Assign adam object to terah object's newly created spouse property
+// Link adam to terah via new spouse property for terah
 
 4. Change the value of the terah weight property to 125.
+// Reassign terah object's weight property to 125
+// Update terah's weight to 125
 
 5. Remove the eyeColor property from terah.
+// Delete terah object's eyeColor property
+// Delete terah's eyeColor property
 
 6. Add a spouse property to adam and assign it the value of terah.
+// Assign terah object to adam object's newly created spouse property
+// Link terah to adam via new spouse property for adam
 
 7. Add a children property to terah and and use object literal notation to assign 
    this variable the value of a JavaScript Object object with no properties
+// Give terah object an empty children property
 
 8. Add a carson property to the value of the terah children property and assign it 
   the value of an object with the property name with a value of "Carson".
+// Add to terah's children property a new object carson with appropriate name value
+// Add carson object (name it) to terah's children property
 
 9. Add a carter property to the value of the terah children property and assign it 
    the value of an object with the property name with a value of "Carter".
+// Add to terah's children property a new object carter with appropriate name value
+// Add carter object (name it) to terah's children property
 
 10. Add a colton property to the value of the terah children property and assign it 
     the value of an object with the property name with a value of "Colton".
+// Add to terah's children property a new object colton with appropriate name value
+// Add colton object (name it) to terah's children property
 
 11. Add a children property to adam and assign it the value of terah children.
+// Assign adam object's newly created children property the same value as terah's
 
 
 */
@@ -48,20 +72,29 @@ var terah = {
 // __________________________________________
 // Write your code below.
 
+// Create adam object
     var adam = {}
+// Name adam object
     adam.name = "Adam";
+// Link adam to terah via new spouse property for terah
     terah.spouse = adam;
+// Update terah's weight to 125
     terah.weight = 125;
+// Delete terah's eyeColor property
     delete terah.eyeColor;
+// Link terah to adam via new spouse property for adam
     terah.spouse.spouse = terah;
+// Give terah object an empty children property
     terah.children = {};
-    var carson = {}
-    carson.name = "Carson";
-    var carter = {}
-    carter.name = "Carter";
-    var colton = {}
-    colton.name = "Colton";
-    terah.children = [carson, colton, carter];
+// Add carson object (name it) to terah's children property
+    terah.children.carson = {name: "Carson"};
+// Add carter object (name it) to terah's children property
+    terah.children.carter = {name: "Carter"};
+// Add colton object (name it) to terah's children property
+    terah.children.colton = {name: "Colton"};
+// Assign adam object's newly created children property the same value as terah's
+    adam.children = terah.children;
+
 
 
 
@@ -72,7 +105,27 @@ var terah = {
 // __________________________________________
 // Reflection: Use the reflection guidelines
 // 
-// 
+/* I like the nature of test-driven development.  One, I like working through each challenge 
+and then letting the code itself tell me what I need to do next.  Two, I think it really 
+solidifies my understanding of the concepts in a very quick way: sometimes I look at the test 
+code, and other times I just figure it out.  I also like the idea of pair-programming where 
+one person writes the code and one person writes the tests.
+
+In general, the concept of objects makes sense to me, though I am sure I will learn more, and 
+be much more confused, as I see more complex objects interacting with each other.  
+
+I had some trouble with challenges 7-10: assigning an empty children property and then 
+filling in the property with objects.  I used Team Treehouse and Code Academy to take general 
+primer courses on JS objects.  I figured out how to do the code through trial and error; I 
+was not able to find a resource to help me find a solution to this specific problem.  
+
+I can definitely appreciate the idea that we need pseudocoding to solve even moderately 
+complicated problems and thus we need to practice pseudocoding now on easier challenges.  
+But it’s frustrating and I fear that my pseudocode is far wordier than I would ever use in a 
+real context.  To me, the idea of pseudocode is to create am algorithm’s framework, to tell 
+why a line/block of code exists where it does.  But here, with these simple challenges, I 
+feel like my pseudocode is more confusing than simply reading the code itself.
+*/
 // 
 // 
 // 
