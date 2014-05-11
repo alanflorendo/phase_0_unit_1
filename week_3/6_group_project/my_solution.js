c7c2532e79925cbd95b2fcdb410f489d0ab5995e
Alan Florendo <alan.florendo@gmail.com>
2014-05-11 16:14:55 -0500

    here goes nothing

---
+++ week_3/6_group_project/my_solution.js
@@ -0,0 +1,29 @@
+3.6:  GROUP PROJECT
+
+I was Person 1 in our group; my job was (i) to convert the test code into user stories, and (ii) to run the 
+test code on the final refactored code submitted to me.  Of the six people in our group, I have met three, 
+have yet to meet two, and am one.  
+
+After having done HTML and CSS for two weeks, this project was my first DBC foray into algorithms.  Since 
+I was Person 1, I focused on getting my user stories out to the group even before I had a real grasp on how 
+JavaScript worked (though I assumed it would be similar to C or Ruby, which I understand somewhat better).  
+As the first and last person in the chain, I knew that everyone had to be done before I could finish my 
+work.
+
+But otherwise, there was not a strong group dynamic for this project.  I worked alone as Person 1.  (The 
+roles of Person 3 and Person 4 were each filled by two persons, four total).  I passed my work along to 
+one person, who never needed to communicate back with me.  And I received my work from one team of two 
+people, and I never needed to work with them.  (I did communicate to the team that our project had been 
+	a success.)  So the project involved a group of people in sequence, but not necessarily together.  
+There were no major issues with communication, power, empathy, etc.
+
+That said, I did find the project very insightful.  In particular, the project emphasizes the role of 
+pseudocode and refactoring in coding.  I definitely appreciate that pseudocoding is necessary: it makes 
+your code readable to others as well as to yourself.  (Even a few days after writing it, your own 
+	uncommented code can be completely bewildering.)  When you’re coding for yourself, I can see that 
+it’s easy to write sloppy pseudocode and still turn it into the right code.  Here in our group project, 
+however, that wouldn’t have been the answer.  In fact, if our coders wanted to write different code than 
+the pseudocode given to them, they had to get the pseudocoder himself to revise the pseudocode beforehand.  
+I’m actually really proud of my team’s ability to go through this process cleanly: state and understand 
+the problem, pseudocode an algorithm, code the pseudocode, and refactor the code.  I appreciate the 
+project’s ability to clarify these important and separate processes.
